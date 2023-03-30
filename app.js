b443f0b5590ac0efdddd30a786bc3f1200b43e3b
Aliko Amos <95168267+Aliko2020@users.noreply.github.com>
2023-03-30 03:32:31 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.btn');
+const fname = document.querySelector('.fname');
+const lname = document.querySelector('.lname');
+const email = document.querySelector('.email');
+const pass = document.querySelector('.password');
+
+button.addEventListener(() =>{
+    pass
+})
+
